abc9cb54324f6cdda09fc186494b0b45c80a448c
issmuth <ismael.mdev@gmail.com>
2024-08-06 17:34:17 +0300

    done

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -6,12 +6,10 @@ $(function() {
 
     $('body').append('<p>Holberton Dashboard</p>');
     $('body').append('<p>Dashboard data for the students</p>');
-    $('body').append('<p>Dashboard data for the students</p>');
     $('body').append('<button>Click here to get started</button>');
     $('body').append('<p id="count"></p>');
     $('body').append('<p>Copyright - Holberton School</p>');
 
-    
     function updateCounter(count) {
         count += 1;
         $('#count').text(`${count} clicks on the button`);
